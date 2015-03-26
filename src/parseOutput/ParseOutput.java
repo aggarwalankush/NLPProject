cb2e032343fb69b7ca8c9cef6d5962674df3d18c
Ankush <ankushagg93@gmail.com>
2015-03-25 22:11:12 -0400

    first commit

---
+++ src/parseOutput/ParseOutput.java
@@ -0,0 +1,48 @@
+package parseOutput;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class ParseOutput {
+
+	public static void main(String[] args) {
+
+		try {
+			new ParseOutput().parse("test-output.txt","parse-output.txt");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void parse(String input, String output) throws Exception {
+
+		BufferedReader br = new BufferedReader(new FileReader(input));
+		BufferedWriter bw=new BufferedWriter(new FileWriter(output));
+		String line = "";
+		int count = 1;
+		br.readLine();
+		while ((line = br.readLine()) != null) {
+			if (line.trim().length() == 0) {
+				count = 1;
+				bw.write("\n");
+				System.out.println();
+				continue;
+			}
+			count++;
+			String[] elements = line.split("\t");
+			bw.write(elements[3]+" ||| ");
+			System.out.print(elements[3] + " ||| ");
+		
+			if (count % 3 != 0) {
+				bw.write(elements[4]+" ||| ");
+				System.out.print(elements[4] + " ||| ");
+			}
+
+		}
+		br.close();
+		bw.close();
+	}
+}
