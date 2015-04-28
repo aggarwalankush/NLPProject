c613390cb3c0841f0d9f7f6176a671417964039e
Ankush <ankushagg93@gmail.com>
2015-04-28 05:18:53 -0400

    generating good matches

--- src/textual_entailment/TextEntail.java
+++ src/textual_entailment/TextEntail.java
@@ -1,7 +1,10 @@
 package textual_entailment;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -9,6 +12,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -17,8 +22,10 @@ import parser.ParseRelation;
 
 public class TextEntail {
 	ParseRelation parseRelation = null;
+	List<String> match_list = new ArrayList<String>();
+
 	public TextEntail() {
-		parseRelation=new ParseRelation();
+		parseRelation = new ParseRelation();
 	}
 
 	public static void main(String[] args) {
@@ -30,17 +37,76 @@ public class TextEntail {
 
 			text_entail.match_document_schema("document_relgram_Relations.txt",
 					"manual_schema_Relations.txt");
-
+			text_entail.match_args_also();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	/*
+	 * Treated equal >>>person == number >>>device == activity
+	 * 
+	 * >>>arg1 arg2 doc_arg3 doc_arg4
+	 * 
+	 * match 1-2 with 3-4 or 4-3 then good match
+	 * 
+	 * >>>arg1 arg2 doc_arg3
+	 * 
+	 * match 1-2 with 3, either match then good match
+	 * 
+	 * >>>arg1 arg2 nothing produced from document
+	 * 
+	 * not a good match
+	 */
+	private void match_args_also() throws Exception {
+		PrintWriter result_file = new PrintWriter(new BufferedWriter(
+				new FileWriter("results.txt", true)));
+		result_file
+				.println("\n--------------------------------------------Good Match--------------------------------------------\n");
+
+		Pattern p = Pattern.compile("\\[(.*?)\\]");
+		Matcher m;
+
+		for (String s : match_list) {
+			StringBuilder sb = new StringBuilder();
+			System.out.println();
+			m = p.matcher(s);
+			while (m.find()) {
+				System.out.print(m.group(1) + "\t");
+				sb.append(m.group(1) + "\t");
+			}
+
+			String[] args = sb.toString().split("\t");
+			int len = args.length;
+			for (int i = 0; i < len; i++) {
+				args[i] = args[i].replaceAll("person", "number");
+				args[i] = args[i].replaceAll("device", "activity");
+			}
+
+			if (len < 3)
+				continue; // no types detected for document line by relgrams
+			else if (len == 3) {// match 0 with 2 or 1 with 2
+				if (args[0].equalsIgnoreCase(args[2])
+						|| args[1].equalsIgnoreCase(args[2]))
+					result_file.println(s);
+			} else if (len == 4) {// match 0-1 with 2-3 or 3-2
+				if ((args[0].equalsIgnoreCase(args[2]) && args[1]
+						.equalsIgnoreCase(args[3]))
+						|| (args[0].equalsIgnoreCase(args[3]) && args[1]
+								.equalsIgnoreCase(args[2])))
+					result_file.println(s);
+			}
+
+		}
+
+		result_file.close();
 	}
 
 	private void match_document_schema(String doc_file, String schema_file)
 			throws Exception {
 		BufferedReader document = new BufferedReader(new FileReader(doc_file));
 		BufferedReader schema = new BufferedReader(new FileReader(schema_file));
-		PrintWriter result_file=new PrintWriter("results.txt");
+		PrintWriter result_file = new PrintWriter("results.txt");
 		String schema_line = null;
 		List<String> schema_list = new ArrayList<String>();
 		while ((schema_line = schema.readLine()) != null)
@@ -48,24 +114,28 @@ public class TextEntail {
 
 		schema.close();
 		String doc_line = null;
+		result_file
+				.println("\n----------------------------Document-Schema pairs matched using only relation----------------------------\n");
 		while ((doc_line = document.readLine()) != null) {
 			String max_match_pair = "";
 			double confidence = 0.0, max_confidence = 0.0;
-			String doc_relation=doc_line.split("\t")[2];
-			doc_relation=parseRelation.parse(doc_relation);
+			String doc_relation = doc_line.split("\t")[2];
+			doc_relation = parseRelation.parse(doc_relation);
 			for (String s : schema_list) {
-				String schema_relation=s.split("\t")[1];
-				schema_relation=parseRelation.parse(schema_relation);
-				
+				String schema_relation = s.split("\t")[1];
+				schema_relation = parseRelation.parse(schema_relation);
+
 				confidence = entail(doc_relation, schema_relation);
 				if (confidence > max_confidence) {
 					max_confidence = confidence;
-					max_match_pair = s + "\t<-schema--|"+ max_confidence +"|--document->\t" + doc_line;
+					max_match_pair = s + "\t<<-schema--|" + max_confidence
+							+ "|--document->>\t" + doc_line;
 				}
 
 			}
-			if (!max_match_pair.trim().isEmpty()){
+			if (!max_match_pair.trim().isEmpty()) {
 				System.out.println(max_match_pair);
+				match_list.add(max_match_pair);
 				result_file.println(max_match_pair);
 			}
 		}
